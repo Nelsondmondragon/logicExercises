a2c221565b4351d3e2b69b2f22184abaafbde6ee
Nelson M <nelsondmondragon@gmail.com>
2022-10-13 22:15:33 -0500

    End of File

---
+++ problem/src/main/java/endofile/Solution.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package endofile;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nelso
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long i = 0;
+        while (sc.hasNext()) {
+            i++;
+            System.out.println(i + " " + sc.nextLine());
+
+        }
+
+    }
+}
